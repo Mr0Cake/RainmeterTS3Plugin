3135a0b8541b01abb8c5b6fef2187e73ba61a537
Mr0Cake <happy_kefke@hotmail.com>
2015-02-24 04:43:39 +0100

    Upload Files
    Rainmeter plugin
    Refresh error

---
+++ PluginEmpty/PluginEmpty.cs
@@ -0,0 +1,180 @@
+﻿// Uncomment these only if you want to export GetString() or ExecuteBang().
+#define DLLEXPORT_GETSTRING
+//#define DLLEXPORT_EXECUTEBANG
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Rainmeter;
+using System.Threading;
+
+
+
+// Overview: This is a blank canvas on which to build your plugin.
+
+// Note: Measure.GetString, Plugin.GetString, Measure.ExecuteBang, and
+// Plugin.ExecuteBang have been commented out. If you need GetString
+// and/or ExecuteBang and you have read what they are used for from the
+// SDK docs, uncomment the function(s). Otherwise leave them commented out
+// (or get rid of them)!qsdf
+
+namespace PluginEmpty
+{
+    internal class Measure
+    {
+        public string RequestedStringValue;
+        public IntPtr SkinHandle;
+        public TeamspeakConnectionThread teamspeakConnection = new TeamspeakConnectionThread();
+        private System.Timers.Timer retryTimer = new System.Timers.Timer(5000);
+
+        internal Measure()
+        {
+        }
+
+        internal void Reload(Rainmeter.API api, ref double maxValue)
+        {
+            SkinHandle = api.GetSkin();
+            retryTimer.Elapsed += tick;
+            retryTimer.Start();
+        }
+
+        internal double Update()
+        {
+            return 0.0;
+        }
+
+
+        //events
+        private void tick(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (teamspeakConnection.Connected != TeamspeakConnectionThread.ConnectionState.Connecting && teamspeakConnection.Connected != TeamspeakConnectionThread.ConnectionState.Connected)
+            {
+                if (teamspeakConnection.ConnectionThread == null || teamspeakConnection.ConnectionThread.ThreadState == ThreadState.Stopped)
+                {
+                    if (TeamspeakConnectionThread.QueryDispatcher != null)
+                    {
+                        
+                    }
+                    teamspeakConnection.startThread();
+                    API.Log(API.LogType.Debug, "Teamspeak.ddl: Start new thread");
+                }
+            }
+            else
+            {
+                retryTimer.Stop();
+            }
+        }
+        
+        
+        
+#if DLLEXPORT_GETSTRING
+        internal string GetString()
+        {
+            string output = "not connected";
+            if (teamspeakConnection.Connected == TeamspeakConnectionThread.ConnectionState.Connected)
+            {
+                 output = teamspeakConnection.ChannelName +
+                    "\r\n" + teamspeakConnection.ChannelClients +
+                    "\r\n" + teamspeakConnection.WhoIsTalking +
+                    "\r\n\r\n" + teamspeakConnection.TextMessage;
+            }
+            else
+            {
+                if (teamspeakConnection.Connected != TeamspeakConnectionThread.ConnectionState.Connecting)
+                {
+                    if (teamspeakConnection.ConnectionThread == null || teamspeakConnection.ConnectionThread.ThreadState == ThreadState.Stopped)
+                    {
+                        if (!retryTimer.Enabled)
+                        {
+                            retryTimer.Start();
+                        }
+                    }
+                }
+                else
+                {
+                    output = "connecting";
+                }
+            }
+            return output;
+            
+        }
+#endif
+        
+#if DLLEXPORT_EXECUTEBANG
+        internal void ExecuteBang(string args)
+        {
+        }
+#endif
+    }
+
+    public static class Plugin
+    {
+#if DLLEXPORT_GETSTRING
+        static IntPtr StringBuffer = IntPtr.Zero;
+#endif
+
+        [DllExport]
+        public static void Initialize(ref IntPtr data, IntPtr rm)
+        {
+            data = GCHandle.ToIntPtr(GCHandle.Alloc(new Measure()));
+        }
+
+        [DllExport]
+        public static void Finalize(IntPtr data)
+        {
+            GCHandle.FromIntPtr(data).Free();
+            
+#if DLLEXPORT_GETSTRING
+            if (StringBuffer != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(StringBuffer);
+                StringBuffer = IntPtr.Zero;
+            }
+#endif
+        }
+
+        [DllExport]
+        public static void Reload(IntPtr data, IntPtr rm, ref double maxValue)
+        {
+            Measure measure = (Measure)GCHandle.FromIntPtr(data).Target;
+            measure.Reload(new Rainmeter.API(rm), ref maxValue);
+        }
+
+        [DllExport]
+        public static double Update(IntPtr data)
+        {
+            Measure measure = (Measure)GCHandle.FromIntPtr(data).Target;
+            return measure.Update();
+        }
+        
+#if DLLEXPORT_GETSTRING
+        [DllExport]
+        public static IntPtr GetString(IntPtr data)
+        {
+            Measure measure = (Measure)GCHandle.FromIntPtr(data).Target;
+            if (StringBuffer != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(StringBuffer);
+                StringBuffer = IntPtr.Zero;
+            }
+
+            string stringValue = measure.GetString();
+            if (stringValue != null)
+            {
+                StringBuffer = Marshal.StringToHGlobalUni(stringValue);
+            }
+
+            return StringBuffer;
+        }
+#endif
+
+#if DLLEXPORT_EXECUTEBANG
+        [DllExport]
+        public static void ExecuteBang(IntPtr data, IntPtr args)
+        {
+            Measure measure = (Measure)GCHandle.FromIntPtr(data).Target;
+            measure.ExecuteBang(Marshal.PtrToStringUni(args));
+        }
+#endif
+    }
+}
---
+++ PluginEmpty/TeamspeakConnectionThread.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Timers;
+using System.Net.Sockets;
+using TS3QueryLib.Core;
+using TS3QueryLib.Core.Client;
+using TS3QueryLib.Core.Client.Entities;
+using TS3QueryLib.Core.Client.Notification.EventArgs;
+using TS3QueryLib.Core.Client.Notification.Enums;
+using TS3QueryLib.Core.Common;
+using TS3QueryLib.Core.Common.Responses;
+using TS3QueryLib.Core.Communication;
+using Rainmeter;
+
+namespace PluginEmpty
+{
+    public class TeamspeakConnectionThread
+    {
+        public Thread ConnectionThread;
+        public TeamspeakConnectionThread()
+        {
+            startThread();
+        }
+
+        public void startThread()
+        {
+            ConnectionThread = new Thread(Connect);
+            ConnectionThread.Start();
+        }
+        
+        public IntPtr SkinHandle;
+        public string FinishAction;
+        //public static Thread ConnectionThread;
+        private static System.Timers.Timer time = new System.Timers.Timer();
+        public readonly object ThreadLocker = new object();
+        public ConnectionState Connected = ConnectionState.Disconnected;
+        public enum ConnectionState
+        {
+            Connecting,
+            Connected,
+            Disconnected
+        }
+        public static AsyncTcpDispatcher QueryDispatcher { get; set; }
+
+        public static QueryRunner QueryRunner { get; set; }
+
+        public string ChannelName = "Not Connected";
+        public string ChannelClients = "";
+        public string WhoIsTalking = "";
+        public string TextMessage = "";
+        public TS3QueryLib.Core.Client.Responses.WhoAmIResponse currentUser { get; set; }
+        public ListResponse<ChannelListEntry> channels { get; set; }
+        public ListResponse<ClientListEntry> clients { get; set; }
+
+        
+
+        private void Connect()
+        {
+            API.Log(API.LogType.Debug, "Teamspeak.ddl: Connecting");
+            // do not connect when already connected or during connection establishing
+            if (QueryDispatcher != null)
+            {
+                API.Log(API.LogType.Debug, "Teamspeak.ddl: QueryDispatcher not null");
+                return;
+            }
+            time.Interval = 10000;
+            time.Elapsed += time_Elapsed;
+            Connected = ConnectionState.Connecting;
+            QueryDispatcher = new AsyncTcpDispatcher("localhost", 25639);
+            QueryDispatcher.BanDetected += QueryDispatcher_BanDetected;
+            QueryDispatcher.ReadyForSendingCommands += QueryDispatcher_ReadyForSendingCommands;
+            QueryDispatcher.ServerClosedConnection += QueryDispatcher_ServerClosedConnection;
+            QueryDispatcher.SocketError += QueryDispatcher_SocketError;
+            QueryDispatcher.NotificationReceived += QueryDispatcher_NotificationReceived;
+            QueryDispatcher.Connect();
+
+        }
+
+        private void time_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            //QueryRunner = new QueryRunner(QueryDispatcher);
+            if (Connected == ConnectionState.Connected)
+            {
+                currentUser = QueryRunner.SendWhoAmI();
+                API.Log(API.LogType.Debug, "Teamspeak.ddl: time_elapsed "+currentUser.ErrorId);
+            }
+            else
+            {
+                
+                //Try again when disconnected
+                if (Connected != TeamspeakConnectionThread.ConnectionState.Connecting)
+                {
+                    ChannelName = "";
+                    ChannelClients = "";
+                    WhoIsTalking = "";
+                    TextMessage = "";
+                    if (QueryRunner != null)
+                        QueryRunner.Dispose();
+
+                    QueryDispatcher = null;
+                    QueryRunner = null;
+                    Connect();
+                    API.Log(API.LogType.Debug, "Teamspeak.ddl: ReConnect");
+                    
+                }
+            }
+
+        }
+
+        private void QueryDispatcher_ReadyForSendingCommands(object sender, System.EventArgs e)
+        {
+            Connected = ConnectionState.Connected;
+            API.Log(API.LogType.Debug, "Teamspeak.ddl: Connected");
+            time.Start();
+            // you can only run commands on the queryrunner when this event has been raised first!
+            QueryRunner = new QueryRunner(QueryDispatcher);
+            do
+            {
+                currentUser = QueryRunner.SendWhoAmI();
+                System.Threading.Thread.Sleep(1000);
+            } while (currentUser.ErrorMessage == "not connected");
+            QueryRunner.Notifications.ChannelTalkStatusChanged += Notifications_ChannelTalkStatusChanged;
+            QueryRunner.Notifications.ClientMoved += Notifications_ClientMoved;
+            QueryRunner.Notifications.MessageReceived += Notifications_MessageReceived;
+            QueryRunner.RegisterForNotifications(ClientNotifyRegisterEvent.Any);
+            updateOutput();
+        }
+
+        private void updateOutput()
+        {
+            currentUser = QueryRunner.SendWhoAmI();
+            channels = QueryRunner.GetChannelList();
+            clients = QueryRunner.GetClientList();
+
+            foreach (ChannelListEntry cle in channels.Values)
+            {
+                if (currentUser.ChannelId == cle.ChannelId)
+                {
+                    ChannelName = cle.Name;
+                    ChannelClients = "";
+                    foreach (ClientListEntry client in clients.Values)
+                    {
+                        if (client.ChannelId == currentUser.ChannelId)
+                        {
+                            ChannelClients += ("\t"+client.Nickname + Environment.NewLine);
+                        }
+                    }
+                    break;
+
+                }
+            }
+            API.Log(API.LogType.Debug, "Teamspeak.ddl: UpdateOutput"+"\r\n"+ChannelName+"\r\n"+ChannelClients);
+
+        }
+
+        private void Notifications_ClientMoved(object sender, TS3QueryLib.Core.Server.Notification.EventArgs.ClientMovedEventArgs e)
+        {
+            updateOutput();
+        }
+
+        private void Notifications_MessageReceived(object sender, TS3QueryLib.Core.Server.Notification.EventArgs.MessageReceivedEventArgs e)
+        {
+            updateOutput();
+        }
+
+        private void QueryDispatcher_ServerClosedConnection(object sender, System.EventArgs e)
+        {
+            // this event is raised when the connection to the server is lost.
+            Connected = ConnectionState.Disconnected;
+            ChannelName = "";
+            ChannelClients = "";
+            WhoIsTalking = "";
+            TextMessage = "";
+        }
+
+        private void QueryDispatcher_BanDetected(object sender, EventArgs<SimpleResponse> e)
+        {
+            Connected = ConnectionState.Disconnected;
+            ChannelName = "Banned";
+        }
+
+        private void QueryDispatcher_SocketError(object sender, SocketErrorEventArgs e)
+        {
+            ChannelName = "";
+            ChannelClients = "";
+            WhoIsTalking = "";
+            TextMessage = "";
+            // do not handle connection lost errors because they are already handled by QueryDispatcher_ServerClosedConnection
+            if (e.SocketError == SocketError.ConnectionReset)
+                return;
+
+            // this event is raised when a socket exception has occured
+            // API.Log(API.LogType.Error, "Teamspeak.ddl: SocketError " + e.SocketError);
+
+            API.Log(API.LogType.Error, "Teamspeak.ddl: socket error" + e.SocketError.ToString());
+            // force disconnect
+            Disconnect();
+        }
+
+        private void QueryDispatcher_NotificationReceived(object sender, EventArgs<string> e)
+        {
+
+        }
+        public Dictionary<uint, string> talking = new Dictionary<uint, string>();
+        private void Notifications_ChannelTalkStatusChanged(object sender, TalkStatusEventArgsBase e)
+        {
+
+            if (e.TalkStatus == TalkStatus.TalkStarted)
+            {
+                foreach (ClientListEntry cle in clients)
+                {
+                    if (e.ClientId == cle.ClientId)
+                    {
+                        if (!talking.ContainsKey(cle.ClientId))
+                            talking.Add(e.ClientId, cle.Nickname);
+                        break;
+                    }
+
+                }
+
+            }
+            else
+            {
+                talking.Remove(e.ClientId);
+            }
+
+            WhoIsTalking = "";
+            foreach (KeyValuePair<uint, string> kvp in talking)
+            {
+                WhoIsTalking += kvp.Value + " ";
+            }
+
+        }
+
+        public void Disconnect()
+        {
+            // QueryRunner disposes the Dispatcher too
+            if (QueryRunner != null)
+                QueryRunner.Dispose();
+            ChannelName = "";
+            ChannelClients = "";
+            WhoIsTalking = "";
+            TextMessage = "";
+            QueryDispatcher = null;
+            QueryRunner = null;
+            Connected = ConnectionState.Disconnected;
+        }
+
+    }
+}
